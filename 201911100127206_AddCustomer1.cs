8b31e0f2da923f9eaffb2a32eeb59f3b9ede7df1
blacha98 <45050808+blacha98@users.noreply.github.com>
2019-11-10 10:59:10 +0100

    Add files via upload

---
+++ 201911100127206_AddCustomer1.cs
@@ -0,0 +1,16 @@
+﻿namespace ZadanieDomowe.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddCustomer1 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace ZadanieDomowe.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ZadanieDomowe.ProdContex>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(ZadanieDomowe.ProdContex context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
